75ca57911c70d3e1e874817a924ba75d6afd79f7
João Silva <joaoramosig@gmail.com>
2022-09-10 23:06:43 +0100

    Added indicator of current selected mode
    The last selected button will now have a different background.
    This is so that the user is able to tell which mode is currently being used.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('mouseover', hoverIn));
 buttons.forEach(button => button.addEventListener('mouseout', hoverOut));
+buttons.forEach(button => button.addEventListener('click', buttonPress));
+
 
 function hoverIn(e) {
     e.target.setAttribute("style", "scale: 1.1");
@@ -8,4 +10,16 @@ function hoverIn(e) {
 
 function hoverOut(e) {
     e.target.setAttribute("style", "scale: 1;");
+}
+
+function buttonPress(e) {
+    updateSelectedBtn(e);
+}
+
+function updateSelectedBtn(e) {
+    buttons.forEach(button => button.classList.remove('selected'));
+    e.target.classList.add('selected');
+    if (e.target.id == 'clear') {
+        e.target.classList.remove('selected');
+    }
 }
